65694d435582cd043ea6d797633d421fc3994862
tihomir tishou78 <tihomir tishou78>
2017-06-02 19:57:36 +0300

    Visual Studio Projects

---
+++ ChooseAdrink2.0/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChooseAdrink2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string profession = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            double totalPrice = 0;
+
+            if(profession == "Athlete")
+            {
+                totalPrice = quantity * 0.70;
+                Console.WriteLine($"The {profession} has to pay {totalPrice:f2}.");
+            }
+            else if (profession == "Businessman" || profession == "Businesswoman")
+            {
+                totalPrice = quantity * 1.00;
+                Console.WriteLine($"The {profession} has to pay {totalPrice:f2}.");
+            }
+            else if (profession == "SoftUni Student")
+            {
+                totalPrice = quantity * 1.70;
+                Console.WriteLine($"The {profession} has to pay {totalPrice:f2}.");
+            }
+            else 
+            {
+                totalPrice = quantity * 1.20;
+                Console.WriteLine($"The {profession} has to pay {totalPrice:f2}.");
+            }
+        }
+    }
+}
